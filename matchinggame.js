3e19db7c5f61e37368431709dc069086d48af2e3
Zac Barnard <barnard.zac@gmail.com>
2017-06-15 16:48:39 -0500

    Initial file upload

---
+++ matchinggame.js
@@ -0,0 +1,23 @@
+//This is the Javascript file for "Project 2 - Javascript Matching Game"
+
+
+ 
+/* Generic Tile
+  //tile x
+  //tile y
+  //tile size (constant, they're square)
+  //tile back imagem, eventually 
+*/
+
+//this function builds the tiles, 
+var Tile = function (x, y,)
+{
+  this.x = x;
+  this.y = y;
+  this.width = 70px;
+  fill(214, 247, 202);
+  strokeWeight(2);
+  rect(this.x, this.y, this.width, this.width, 10);
+};
+
+var testTile = new Tile(50, 50)
